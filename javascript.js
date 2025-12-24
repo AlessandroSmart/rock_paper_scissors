6eeeb4d14ea591f1dd2cc7d99925606fb19eec38
Alessandro <61292924+AlessandroSmart@users.noreply.github.com>
2025-12-24 17:44:14 -0500

    added rps ui

--- javascript.js
+++ javascript.js
@@ -17,32 +17,48 @@ let getHumanChoice = function() {
 let humanScore = 0;
 let computerScore = 0;
 
+const statuss = document.querySelector(".comment");
+const pScore = document.querySelector(".pscore");
+const cScore = document.querySelector(".cscore");
+
 function playRound(humanChoice, computerChoice) {
-    let hChoice = humanChoice();
-    let cChoice = computerChoice();
+    let hChoice = humanChoice.toLowerCase();
+    let cChoice = computerChoice.toLowerCase();
     if(hChoice == cChoice) {
-        console.log("Tie!");
-        console.log(`You picked ${hChoice} and the computer picked
-         ${hChoice} `);
+        statuss.textContent = ` Tie! You picked ${hChoice} and the computer picked
+         ${hChoice}`;
     }
     else if ((hChoice === "rock" && cChoice === "scissors")
         || (hChoice === "scissors" && cChoice === "paper")
         || (hChoice === "paper" && cChoice === "rock")) 
         {
             humanScore++;
-            console.log(`You win! ${hChoice} beats ${cChoice}`);
+            pScore.textContent = `Your score is ${humanScore}`;
+            cScore.textContent = `The computer score is ${computerScore}`
+            statuss.textContent = `You win! ${hChoice} beats ${cChoice}`
         }
         else {
             computerScore++;
-            console.log(`You lose! ${cChoice} beats ${hChoice}`);
+            pScore.textContent = `Your score is ${humanScore}`;
+            cScore.textContent = `The computer score is ${computerScore}`
+            statuss.textContent = `You lose! ${cChoice} beats ${hChoice}`
         }
 
+}
 
-}
-function playGame() {
-   for(let i = 1; i <=5; i++) {
-     playRound(getHumanChoice, getComputerChoice);
-   }
-   console.log(`Your final score is ${humanScore} vs ${computerScore}`);
-}
-playGame();
+// EVENT DELEGATION
+const selection = document.querySelector(".selection");
+selection.addEventListener("click", (e) => {
+  if(!e.target.matches("button")) return;
+
+  const playerSelection = e.target.className; 
+  playRound(playerSelection, getComputerChoice());
+
+  if (humanScore === 5 || computerScore === 5) {
+    selection.style.pointerEvents = "none";
+    const winner = (humanScore === 5 ) ? 'You won! Congrats' : 'You lost! Good luck next time';
+    statuss.textContent = winner;
+  }
+});
+
+
